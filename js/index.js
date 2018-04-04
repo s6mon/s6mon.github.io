f292e0b7f9ba9a1c4a22f74359c263f89ab5d7a1
s6mon <sim.chambonnet@gmail.com>
2018-04-04 13:38:53 +0200

    re

--- js/index.js
+++ js/index.js
@@ -1,50 +1,5 @@
 
 
-document.getElementById("all").addEventListener("mousemouve", mouseOverHeader());
-document.getElementById("header").addEventListener("mouseout", mouseOutHeader());
-
-
-// function mouseMoveAll(e) {
-//     var divHeader = document.getElementById("header");
-//     mOver = isMouseOver(divHeader);
-//     divHeader.style.display = "none";
-
-//     // if(mOver) {
-//     //     divHeader.style.display = "block";
-//     // }
-//     // else if (!mOver) {
-//     //     // console.log("on est pas display");
-
-//     //     divHeader.style.display = "none";
-//     //     setTimeout (function() {divHeader.style.display = "none"; }, 2000 );
-
-//     // }
-//     // divHeader.style.display = "block";
-    
-// }
-
-function mouseOverHeader(e) {
-    console.log("LAAAAA");
-    var divHeader = document.getElementById("header");
-    divHeader.style.display = "block";
-}
-
-function mouseOutHeader(e) {
-    var x = document.getElementById("header");
-    x.style.display = "none";
-}
-
-function isMouseOver(element) {
-
-    
-    // element.style.display = "block";
-    // element.onmouseover = function() { element.style.display = "block"; };
-    // element.onmouseout = function() {
-    //     setTimeout (function() {element.style.display = "none"; }, 2000 );
-    // };
-
-}
-
 function mouseClic(element, textID) {
     var para = document.getElementById(textID);
     if (para.style.display === "block") {
@@ -53,4 +8,3 @@ function mouseClic(element, textID) {
         para.style.display = "block";
     }
 }
-
