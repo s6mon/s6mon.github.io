32f995d7db9bf4c4db02c1b71611a3f4bdab6b16
s6mon <sim.chambonnet@gmail.com>
2018-04-05 15:52:39 +0200

    picture filling + styleguide

--- js/index.js
+++ js/index.js
@@ -1,10 +1,52 @@
 
 
+
 function mouseClic(element, textID) {
     var para = document.getElementById(textID);
     if (para.style.display === "block") {
         para.style.display = "none";
     } else {
         para.style.display = "block";
+    }
+}
+
+
+function myFunction() {
+    i = 0;
+    var image = document.getElementById("pngFilling");
+    var elementTextHere = document.getElementById("textHere");
+    elementTextHere.innerHTML = "Salut";
+    Loadgo.init(image, {
+        'direction': 'bt'
+      });
+     var ok = Loadgo.options(image);
+    console.log(ok);
+
+}
+var i = 0;
+function start() {
+    var image = document.getElementById("pngFilling");
+    var elementTextHere = document.getElementById("textHere");
+    elementTextHere.innerHTML = "Salut";
+    i = i + 10;
+    Loadgo.setprogress(image, i);
+    var ok = Loadgo.options(image);
+    console.log(ok);
+}
+
+function move() {
+    var elem = document.getElementById("myBar");
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        }
+        else {
+            width++;
+            console.log(width);
+            elem.style.width = width + '%';
+            elem.innerHTML = width * 1 + '%';
+        }
     }
 }
