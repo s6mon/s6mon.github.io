21a23ba4ff20edad150c3e39c2fe44ec68771d58
s6mon <sim.chambonnet@gmail.com>
2018-04-24 09:24:50 +0200

    last

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 
-var textS_H = ["text1", "text2", "text3", "text4", "text1Faq", "text2Faq", "text3Faq", "text4Faq"];
+var textS_H = ["p1_s1_1_2", "p1_s1_2_2", "p1_s1_3_2", "p1_s1_4_2", "p6_s1_2", "p6_s2_2", "p6_s3_2", "p6_s4_2"];
 var lang;
 var isDetected;
-var langSupport = ["en"];
+var langSupport = {en:'', se:''};
+var nbDayToElection;
+const mealsGoal = 10000;
 
 function myFunction() {
     var x = document.getElementById("demo");
@@ -28,7 +30,8 @@ function w3_close(clic) {
 
 function circleMeals() {
     var nbLunches = getNbLUnches();
-    console.log("this the number of ... : " + nbLunches);
+    nbLunches = 5000; //TO REMOVE
+
     var cM = document.getElementById("rond");
     cM.style.border = "4px solid black";
     var circleS = window.innerWidth / 2;
@@ -37,7 +40,46 @@ function circleMeals() {
 
     var diskMeal = document.getElementById("taux");
     diskMeal.style.width = circleS + "px";
-    diskMeal.style.height = "50%";
+    diskMeal.style.height = (nbLunches / mealsGoal) * 100 + "%";
+
+    var nbMealsAte = document.getElementById("nbMealsAte");
+    var mealsText = document.getElementById("p3_fig1_2");
+    var mealsNbPos = document.getElementById("mealsNbPos");
+
+    nbMealsAte.innerHTML = nbLunches;
+    nbMealsAte.style.marginLeft = ((nbMealsAte.parentNode.parentNode.offsetWidth / 2) - (nbMealsAte.offsetWidth/2)) + "px";
+    mealsText.style.marginLeft = "70px";
+
+    if(nbLunches < 5000){
+        mealsNbPos.style.bottom = "40%";
+    }
+    else{
+        mealsNbPos.style.bottom = "-5%";
+        mealsNbPos.style.color = "#ffdc00";
+    }
+}
+
+function calculateDayToElection() {
+    var electionD = new Date(2018, 8, 9, 23, 0, 0, 0);
+    var today = new Date();
+    today = today.getTime();
+    var diff = electionD - today;
+    diff = diff / (1000 * 60 * 60 * 24);
+    nbDayToElection = Math.floor(diff);
+    return nbDayToElection;
+}
+
+function printToElection() {
+    var nbDayToPrint = calculateDayToElection();
+    document.getElementById("nbDayToElection").innerHTML = nbDayToPrint;
+}
+
+function printmealByDay() {
+    var nbDayBeforeElection = calculateDayToElection();
+    var nbLunches = getNbLUnches();
+    nbLunches = 5000;
+    var mealsByDay = Math.round((mealsGoal - nbLunches) / nbDayToElection);
+    document.getElementById("nbMealsByDay").innerHTML = mealsByDay;
 }
 
 /*
@@ -49,6 +91,7 @@ param2 : textID the of text show/hide
 function scrollingText(element, textID) {
     hideText(textID, textS_H);
     var para = document.getElementById(textID);
+    console.log(para);
     if (para.style.display === "block") {
         para.style.display = "none";
     } else {
@@ -85,33 +128,35 @@ function windowResizing() {
     width = window.innerWidth;
     height = window.innerHeight;
 
-    var tenThLuncher = document.getElementById("tenThLuncher");
-    var subTenThL = document.getElementById("sub-10000luncher");
+    var p1_Title = document.getElementById("p1_Title");
+    var subTenThL = document.getElementById("p1_SubTitle");
 
     if(width < 576) {
 
     }
     else {
-        tenThLuncher.style.fontSize = "40px";
+        p1_Title.style.fontSize = "40px";
         subTenThL.style.fontSize = "35px";
     }
 }
 
 
 function loadJson(strPage, isFirstCall) {
-    // if (isFirstCall){
-    //     adaptLang();
-    // } else {
-    //     lang='en';
-    // }
-    adaptLang();
-    if (adaptLang in langSupport){
-        lang = adaptLang;
+
+    var adaptLangue = adaptLang();
+    if (adaptLangue in langSupport){
+        console.log("on est dans if");
+        lang = adaptLangue;
     }
     else {
         lang = 'en';
     }
-    var jsonFile = '../translate/'+strPage+'_'+lang+'.json';
+    if(strPage == 'index'){
+        dir1 = "";
+    }else {
+        dir1 = "../"
+    }
+    var jsonFile = dir1+'translate/'+strPage+'_'+lang+'.json';
     console.log('Which json are selected : ' + jsonFile);
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.overrideMimeType("application/json");
@@ -120,9 +165,10 @@ function loadJson(strPage, isFirstCall) {
         if (this.readyState == 4 && this.status == 200){
             var myObjText = xmlhttp.responseText;
             var myObj = JSON.parse(myObjText);
-            overallElem = myObj.page;
+            var overallElem = myObj.page;
             for (x in overallElem) {
                 var currentElem = overallElem[x];
+                console.log(currentElem);
                 if (currentElem.tag === "") {
                     document.getElementById(currentElem.id).innerHTML = currentElem.content;
                 }
@@ -133,6 +179,13 @@ function loadJson(strPage, isFirstCall) {
                     newLink.href =  currentElem.url;
                     newLink.style.textDecoration = "underline";
                     linkDiv.appendChild(newLink);
+                }
+                else if (currentElem.tag === "p"){
+                    var div = document.getElementById(currentElem.id);
+                    var newNode = document.createElement("p");
+                    var textNode = document.createTextNode(currentElem.content);
+                    newNode.appendChild(textNode);
+                    div.appendChild(newNode);
                 }
             }
         }
@@ -145,8 +198,8 @@ function loadJson(strPage, isFirstCall) {
 
 function adaptLang() {
     var usrLang = navigator.language || navigator.userLanguage; 
-    lang = usrLang;
     document.documentElement.lang = usrLang;
+    return usrLang;
 }
 
 
